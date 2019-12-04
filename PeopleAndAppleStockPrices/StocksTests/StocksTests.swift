436ca0ec0c92cc220c4b8def7fe4da51acd8dbe6
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-12-04 14:18:26 -0500

    first cicommit

---
+++ PeopleAndAppleStockPrices/StocksTests/StocksTests.swift
@@ -0,0 +1,34 @@
+//
+//  StocksTests.swift
+//  StocksTests
+//
+//  Created by Amy Alsaydi on 12/4/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import XCTest
+// add @testable to get access to the json file 
+
+class StocksTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
